5e4055140930773ae33a2bfeb23c582c668eef1d
aswin16 <vaswin0137@gmail.com>
2020-11-24 22:33:22 +0530

    doc

--- temp_main.c
+++ temp_main.c
@@ -4,7 +4,7 @@
 #include<math.h>
 #include<time.h>
 
-const int N=100;
+const int N=50;
 const float J=1;
 const float T=2;
 
@@ -30,14 +30,14 @@ for(int i=0;i<N;i++)
 
 return(mag/(N*N));
 }
+/****************************/
 
-//energy difference for each prespective spin flip
- double  deltaE(int lattice[N][N],int i,int j)
-{ 
+/*energy difference for each prospective spin flip 
+periodic boundary condition ***/
 
+double  deltaE(int lattice[N][N],int i,int j){ 
 
-
-return( 2*J*lattice[i%N][j%N]*( lattice[i%N][(j+1)%N]+lattice[i%N][(j-1)%N]+lattice[(i+1)%N][j%N]+lattice[(i-1)%N][j%N] ) );
+			return(2*J*lattice[i%N][j%N]*( lattice[i%N][(j+1)%N]+lattice[i%N][(j-1)%N]+lattice[(i+1)%N][j%N]+lattice[(i-1)%N][j%N] ));
 
 
       }
@@ -47,109 +47,98 @@ return( 2*J*lattice[i%N][j%N]*( lattice[i%N][(j+1)%N]+lattice[i%N][(j-1)%N]+latt
 
 /**********evolve**********/
 
-void evolve(int present[N][N])
+void evolve(int present[N][N]){
+	  
+	  long unsigned int i,j;
+	  double ediff;
+	  int counter;
+     long unsigned int k=1;
 
+     //long double kb=1.38*pow(10,-23);
+     int n=N-1;
 
-{
-long unsigned int i,j;
-
- double ediff;
-int counter;
-long unsigned int k=1;
-
-//long double kb=1.38*pow(10,-23);
-int n=N-1;
-
-long unsigned int max,rand,m;
-max=4294967295;
-long double kb=1;
-long double prob;
-//unsigned long s;
-time_t seconds; 
+     long unsigned int max,rand,m;
+	  max=4294967295;
+	  long double kb=1;
+     long double prob;
+     //unsigned long s;
+     time_t seconds; 
 
 
 
 
 
 
-//FILE *file;
-//file = fopen ("data450_T=1.txt","w");
+	  //FILE *file;
+     //file = fopen ("data450_T=1.txt","w");
 
 
 
 
 
 
-    while(k>0)
-{  
-    seconds = time(NULL); //seeding mtwister
-//seconds=seconds+k;
-init_genrand(seconds+k);
+    while(k>0){      //time steps
+    
+	 		 seconds = time(NULL); //seeding mtwister
+			 //seconds=seconds+k;
+          init_genrand(seconds+k);
 
   
-m=max%(n+1);
+          m=max%(n+1);
 
 
-do{rand=genrand_int32(); }  //casting the random no genearator to a range ,naive and wrong implementation ,needs refinement
-  while(rand>=max-m);
-  i=rand%(n+1);
-
-
-
-do{rand=genrand_int32();}
-  while(rand>=max-m);
-   j=rand%(n+1);
-
-
-ediff=deltaE( present,i,j);
+          do{rand=genrand_int32(); }//casting the random no genearator to a range ,naive and wrong implementation ,needs refinement
+          while(rand>=max-m);
+                i=rand%(n+1);
 
 
 
-//double r= genrand_real1(); //random real number in [0,1]
+          do{rand=genrand_int32();}
+          while(rand>=max-m);
+                j=rand%(n+1);
 
-//prob=expl(-(Ediff)/(kb*T));
 
-
-
-if( ediff < 0 ||  genrand_real1() < expl(-ediff/T))
-
-          {  present[i][j]=-present[i][j];}
+          ediff=deltaE( present,i,j);
 
 
 
+          //double r= genrand_real1(); //random real number in [0,1]
+
+          //prob=expl(-(Ediff)/(kb*T));
 
 
 
+         if( ediff < 0 ||  genrand_real1() < expl(-ediff/T))  //spin-flipping with a probability using boltzaman factor
 
-//fprintf (file, "spins flipped= %lu\tmagnetization=%Lf\n",k,magnetization);
- 
-
-//printf("\n");
+            {  present[i][j]=-present[i][j];}
 
 
+         //fprintf (file, "spins flipped= %lu\tmagnetization=%Lf\n",k,magnetization);
+         //printf("\n");
 
-printf("\033[H\033[J"); //clearing the screen
+         //printf("\033[H\033[J"); //clearing the screen
 
 
+          //printf("\033[H\033[J"); //clearing the screen
+			for(i=0;i<N;i++){     //printing the matrix
+			   counter=0;
+		      
+				for(j=0;j<N;j++) {
 
-for(i=0;i<N;i++)     //printing the matrix
-          {   counter=0;
-		  for(j=0;j<N;j++)
-
-                  {if(present[i][j]==1)  printf("\033[07m  \033[m");
-			  else printf("  ");	  //printf("\t%d",next[i][j]);
-			  counter++;
-			  if (counter==N){ printf("\n");}  // printf("\n");printf("\n");}
+                if(present[i][j]==1)  printf("\033[07m  \033[m");
+			       else printf("  ");	  //printf("\t%d",next[i][j]);
+			       counter++;
+			       if (counter==N){ printf("\n");}  // printf("\n");printf("\n");}
 		  }
 
            } 
 
-//printf("i=%lu\tj=%lu\tgeneration=%lu\tEdiff=%Lf\tmagnetization=%Lf",i,j,k,Ediff,magnetization);
-//count++;
-k++;
+		//printf("i=%lu\tj=%lu\tgeneration=%lu\tEdiff=%Lf\tmagnetization=%Lf",i,j,k,Ediff,magnetization);
+      //count++;
+      k++;
+		//usleep(99000);
+		printf("\033[H\033[J"); //clearing the screen
 
-
-usleep(9100);
 }
 //fclose(file);
 
@@ -159,31 +148,33 @@ usleep(9100);
 
 
 
-int main()
+int main(){
 
-{
-time_t seconds;
+	 time_t seconds;
 
     seconds = time(NULL); //seeding mtwister
-init_genrand(seconds);
+	 init_genrand(seconds);
 
 
 
-int RANDOM[N][N];
-int i,j;
+	 int RANDOM[N][N];
+    int i,j;
+    
 
-for(i=0;i<N;i++)
+   // generating a lattice randomly
 
-  {for(j=0;j<N;j++)
-	{  unsigned long ran=genrand_int32();
+    for(i=0;i<N;i++) {
 
-           if(ran%2==0) RANDOM[i][j]=1;
-           else RANDOM[i][j]=-1;
+  		  for(j=0;j<N;j++) {
+	         unsigned long ran=genrand_int32();
+				if(ran%2==0) RANDOM[i][j]=1;
+            else RANDOM[i][j]=-1;
              
-        }
-}
-//printf("%Lf",energy(RANDOM));
- evolve(RANDOM);
+            }
+   } 
+    printf("\033[H\033[J"); //clearing the screen
+
+    evolve(RANDOM);
 }
 
 
